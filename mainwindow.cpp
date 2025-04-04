3ad4495776ca143ffe4d01069d698e3b86d09fe1
Francesco Paolo Luca Zanellato <fraz333@gmail.com>
2025-04-04 18:53:01 +0200

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,22 @@
+/*
+ * Author: Francesco Paolo Luca Zanellato
+ * Copyright (C) 2015-2025 Francesco Paolo Luca Zanellato
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 //LICENSE: ElectricalCalculator version 0.0.1
-//LICENSE: © Francesco Zanellato 2024
-//LICENSE: This program is freeware; it is provided "AS IT IS", without any warranty."
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QMessageBox>
